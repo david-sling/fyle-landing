266bd2508c1e83ceb4a57071b073e28cf5783c36
David S D <sddavid.johan@gmail.com>
2021-08-19 13:59:48 +0530

    Mobile optimised

--- src/sections/Feedback.jsx
+++ src/sections/Feedback.jsx
@@ -4,7 +4,7 @@ export default function Feedback() {
   return (
     <section className="Feedback">
       <h2>client's feedback</h2>
-      <h1>people say's about us !</h1>
+      <h1>people say's about us!</h1>
       <p>
         Jannat Tumpa The standard chunk of Lorem Ipsum used since the 1500s is
         reproduced below for those interested. Sections Bonorum et Malorum
