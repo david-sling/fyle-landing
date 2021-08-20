636cf2e56893aa00906ddd8144d1bdeb32952a34
David S D <sddavid.johan@gmail.com>
2021-08-20 19:16:28 +0530

    CSS improvements

--- src/config/album.js
+++ src/config/album.js
@@ -5,19 +5,19 @@ import album3 from "../assets/album-3.jpg";
 const album = [
   {
     image: album1,
-    title: "Title 1",
+    title: "Genderless Kei – Japan’s Hot",
     description:
       "Set to launch on the manufacturer’s new A330neo aircraft in 2017, it’s offering lots of",
   },
   {
     image: album2,
-    title: "Title 2",
+    title: "Better Strategy & Quality",
     description:
       "Set to launch on the manufacturer’s new A330neo aircraft in 2017, it’s offering lots of",
   },
   {
     image: album3,
-    title: "Title 3",
+    title: "Genderless Kei – Japan’s Hot",
     description:
       "Set to launch on the manufacturer’s new A330neo aircraft in 2017, it’s offering lots of",
   },
--- src/sections/Feedback.jsx
+++ src/sections/Feedback.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { ReactComponent as Quotes } from "../assets/quotes.svg";
 
 export default function Feedback() {
   return (
     <section className="Feedback">
       <h2>client's feedback</h2>
       <h1>people say's about us!</h1>
-      <p>
-        Jannat Tumpa The standard chunk of Lorem Ipsum used since the 1500s is
-        reproduced below for those interested. Sections Bonorum et Malorum
-        original.
-      </p>
+      <div className="quote">
+        <Quotes />
+        <p>
+          Jannat Tumpa The standard chunk of Lorem Ipsum used since the 1500s is
+          reproduced below for those interested. Sections Bonorum et Malorum
+          original.
+        </p>
+      </div>
       <h6>
         <span>JANNAT TUMPA</span> - COO, AMERIMAR ENTERPRISES, INC.
       </h6>
